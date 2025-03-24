66071c3857c3b16cc8c405d0bc2227dbb5ea7d28
moo611 <lnpjjds@gmail.com>
2025-03-24 13:33:55 +0800

    fix

--- src/views/record/index.vue
+++ src/views/record/index.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
     <div class="header">
-      <el-button v-show="false" type="primary" @click="getRecordList">查询</el-button>
+      <div v-show="getUser().role == '0'">
+        <el-date-picker  value-format="YYYY-MM-DD" v-model="queryParams.checkOutTime"
+        type="date" placeholder="请选择" />
+      </div>
+      
+      <el-button style="margin-left: 20px;" v-show="getUser().role == '0'" type="primary" @click="getRecordList">查询</el-button>
       <el-button v-show="getUser().role == '1'" @click="handleAdd" type="primary" class="btn-add">打卡</el-button>
+
+      <div class="right" v-show="getUser().role == '0'">
+        <span style="margin: 10px;font-size: 14px;">打卡人数：{{ state.data.total }}</span>
+        <span style="margin: 10px;font-size: 14px;">未打卡人数：{{ state.left }}</span>
+        <span style="margin: 10px;font-size: 14px;">总人数：{{ state.total }}</span>
+      </div>
+
     </div>
     <el-table class="my-table" :data="state.data.list">
 
@@ -25,30 +37,62 @@ import axios from '../../axios';
 import { ElMessage } from 'element-plus';
 import { getUser } from '../../utils/auth';
 
+
+const getToday = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0'); // 月份从 0 开始，需要 +1
+  const day = String(today.getDate()).padStart(2, '0');
+
+  const formattedDate = `${year}-${month}-${day}`;
+  console.log(formattedDate); // 例如：2023-10-05
+  return formattedDate
+}
+
+const getTotal = () => {
+
+  axios.get('user/list', { params: {pageNum:1,pageSize:10000,role:'1'} }).then(res => {
+    console.log(res)
+    state.total = res.total
+    state.left = state.total-state.data.total
+    
+
+  })
+
+}
+
 const queryParams = reactive({
   pageNum: 1,
   pageSize: 10,
-  createBy:''
+  createBy: '',
+  checkOutTime: ''
 })
+if (getUser().role == '0') {
+  queryParams.checkOutTime = getToday()
+}
 
 const state = reactive({
   data: {},
-  curRecord: {}
+  curRecord: {},
+  total: 0,
+  left: 0
 })
 
 
 
 const getRecordList = () => {
-  if(getUser().role == '0'){
+  if (getUser().role == '0') {
     queryParams.createBy = ''
-  }else{
+
+  } else {
     queryParams.createBy = getUser().username
   }
-  axios.get('record/list', { params: queryParams }).then(res => {
-
+  axios.post('record/list', queryParams).then(res => {
+    
     state.data = res
 
     console.log(state.data)
+    getTotal()
 
   })
 
@@ -59,7 +103,7 @@ const getRecordList = () => {
 const handleAdd = () => {
   axios.post('record/registry').then(res => {
 
-    
+
     ElMessage.success('打卡成功')
     getRecordList()
 
@@ -83,6 +127,11 @@ getRecordList()
   align-items: center;
   /* margin-bottom: 30px; */
 
+}
+
+.right {
+  position: absolute;
+  right: 0;
 }
 
 .btn-add {
